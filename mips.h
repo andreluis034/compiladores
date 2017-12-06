51d021ad4300ca19055c22f195587ff5dce1d2cd
Arukiap <33361409+Arukiap@users.noreply.github.com>
2017-12-06 18:40:58 +0000

    LindaVariavel onde andas tu amole

--- mips.h
+++ mips.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "threeAddressCode.h"
+#include "list.h"
+
+typedef List VariableList;
+
+VariableList* makeVariableList(char* firstVariable);
+VariableList* appendVariable(VariableList* list, char* variable);
+VariableList* prependVariable(VariableList* list, char* variable);
+char* getVariable(VariableList* list);
 
 void compileToMips(InstList* instructionList,CmdList* cmdlist) ;
 void initializeRegisters() ;
-void getVariables(CmdList* cmdlist) ;
+void checkDeclaration(Cmd* cmd) ;
+void checkIfElse(Cmd* cmd) ;
+void checkFor(Cmd* cmd) ;
+void checkFunc(Cmd* cmd) ;
+void checkCmd(Cmd* cmd) ;
+void checkCmdList(CmdList* cmdlist);
