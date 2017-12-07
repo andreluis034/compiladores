b603ea3adb0bfd381d4f6b472d9d403aa1439b5c
andreluis034 <andreluis034@users.noreply.github.com>
2017-12-07 16:58:13 +0000

    move variable finder

--- mips.h
+++ mips.h
@@ -2,18 +2,7 @@
 #include "threeAddressCode.h"
 #include "list.h"
 
-typedef List VariableList;
 
-VariableList* makeVariableList(char* firstVariable);
-VariableList* appendVariable(VariableList* list, char* variable);
-VariableList* prependVariable(VariableList* list, char* variable);
-char* getVariable(VariableList* list);
 
 void compileToMips(InstList* instructionList,CmdList* cmdlist) ;
 void initializeRegisters() ;
-void checkDeclaration(Cmd* cmd) ;
-void checkIfElse(Cmd* cmd) ;
-void checkFor(Cmd* cmd) ;
-void checkFunc(Cmd* cmd) ;
-void checkCmd(Cmd* cmd) ;
-void checkCmdList(CmdList* cmdlist);
